75c69b9b0b7793a8d01581383fe3bab3e9d83b8d
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-09-09 20:26:35 -0600

    Created tables sql and in models, seeded in sql

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,35 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement:true
+    },
+    product_name:{
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate:{
+        isDecimal: true,
+      }
+    },
+    stock:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate:{
+        isNumeric: true,
+      }
+    },
+    category_id:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+
   },
   {
     sequelize,
