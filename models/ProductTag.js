ffa50ddce7d4cc39c55472f2b6e76cf7d6c47f4e
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-09-13 09:22:02 -0600

    added reference into product

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -16,9 +16,19 @@ ProductTag.init(
     },
     product_id:{
       type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+        unique:false 
+      }
     },
     tag_id:{
       type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+        unique:false 
+      }
     }
   },
   {
